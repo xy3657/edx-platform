185f543653da76806b16780fa50db28f7b1b0e74
Michael Youngstrom <myoungstrom@edx.org>
2018-05-03 11:14:15 -0400

    stuff it with some phony stuff

--- scripts/dummy-script.sh
+++ scripts/dummy-script.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-echo hey
+# Violations thresholds for failing the build
+source scripts/thresholds.sh
+
+XSSLINT_THRESHOLDS=`cat scripts/xsslint_thresholds.json`
+export XSSLINT_THRESHOLDS=${XSSLINT_THRESHOLDS//[[:space:]]/}
