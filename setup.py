671182a107bd002fa469a0885691bd73f7e9d7b7
David Ormsbee <dave@edx.org>
2018-05-09 09:07:54 -0400

    Add forums common app to Studio.
    This is necessary for forums to catch the course_published signal and
    generate its discussion_id_map data.

--- setup.py
+++ setup.py
@@ -79,6 +79,7 @@ setup(
         ],
         "cms.djangoapp": [
             "ace_common = openedx.core.djangoapps.ace_common.apps:AceCommonConfig",
+            "discussion = lms.djangoapps.discussion.apps:DiscussionConfig",
             "plugins = openedx.core.djangoapps.plugins.apps:PluginsConfig",
             "schedules = openedx.core.djangoapps.schedules.apps:SchedulesConfig",
             "theming = openedx.core.djangoapps.theming.apps:ThemingConfig",
